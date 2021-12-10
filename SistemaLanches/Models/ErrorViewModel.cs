f5345d2d445cbafff030219e6d082fe5c009f93e
Pedro Machado Pestka <pedro.pestka@hotmail.com>
2021-12-10 07:42:36 -0300

    first commit

---
+++ SistemaLanches/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace SistemaLanches.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
